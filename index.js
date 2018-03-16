decba0b51a974fd1619a693fda9ea36939bbcf92
Ai Yen Tam <tam.aiyen@gmail.com>
2018-03-16 16:45:12 -0400

    linking front-end and back-end

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const cors = require("cors");
 const gifController = require("./controllers/gifs");
 const app = express();
 
+const mongoose = require("./models/gif");
+const Gifs = mongoose.model("Gifs");
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/gifs", gifController);
+app.get(
+  ("/gifs",
+  (req, res) => {
+    Gifs.find()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+);
 
 app.listen(3000, () => console.log("I am running"));
